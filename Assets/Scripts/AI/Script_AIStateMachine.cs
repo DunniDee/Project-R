96f5f0338c6296761308a960a27e7e999340fe09
Ashurri <Ashley.rickit@gmail.com>
2022-04-18 14:07:39 +1200

    AI State Machine
    Current States: Idle, Death, Chase Player

---
+++ Assets/Scripts/AI/Script_AIStateMachine.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_AIStateMachine
+{
+    public AIState[] States;
+    public Script_BaseAI agent;
+    public AIStateID currentStateID;
+    public Script_AIStateMachine(Script_BaseAI _agent){
+        this.agent = _agent;
+        int numStates = System.Enum.GetNames(typeof(AIStateID)).Length;
+        States = new AIState[numStates];
+    }
+
+    public void RegisterState(AIState state)
+    {
+        int index = (int)state.getID();
+        States[index] = state;
+    }
+
+    public AIState GetState(AIStateID stateID){
+        return States[(int)stateID]; 
+    }
+    
+    public void Update(){
+        GetState(currentStateID)?.Update(agent);
+    }
+
+    public void ChangeState(AIStateID newStateID){
+        GetState(currentStateID)?.Exit(agent);
+        currentStateID = newStateID;
+        GetState(currentStateID)?.Enter(agent);
+    }
+     
+}
