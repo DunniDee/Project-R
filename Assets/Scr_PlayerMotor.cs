c80342750206da37a3defa4381b712b222b84616
Dunstan <dunstanbdalin@gmail.com>
2022-07-19 15:09:48 +1200

    Pushing no

--- Assets/Scr_PlayerMotor.cs
+++ Assets/Scr_PlayerMotor.cs
@@ -4,61 +4,151 @@ using UnityEngine;
 
 public class Scr_PlayerMotor : MonoBehaviour
 {
+    [Header("Core Components")]
     [SerializeField] CharacterController Movment;
     [SerializeField] Transform Orientation;
+    [SerializeField] LayerMask GroundMask;
+    [Space]
 
-
-    [SerializeField] float WalkSpeed;
-    [SerializeField] float SprintSpeed;
+    [Header("Movement Stats")]
+    [SerializeField] float MoveSpeed;
+    [SerializeField] float AirSpeed;
     [SerializeField] float Acceleration;
+    [SerializeField] float m_MomentumAcceleration;
+    [SerializeField] float m_MomentumDecay;
     float m_MovementSpeed;
-    bool m_IsSprinting;
+    [Space]
+
+    [Header("Jump Stats")]
+    [SerializeField] float m_Gravity;
+    [SerializeField] float m_JumpMomentum;
+    [SerializeField] float m_JumpHeight;
+
+    //Motor Status Bools
+    public bool m_IsGrounded;
 
     private Vector3 m_MoveDirection;
     private Vector3 m_SmoothMoveDirection;
+    public Vector3 m_MomentumDirection;
+    private Vector3 m_VerticalVelocity;
     float m_ForwardMovement;
     float m_SidewardMovement;
+
+    [Header("GameFeel")]
+    [SerializeField] Scr_CameraEffects CamEffects;
+    bool m_WasGrounded;
+    float LastYVelocity;
 
     // Update is called once per frame
     void Update()
     {
-        GetInput();
+        GetMovementInput();
+        CheckGround();
+
+        Jump();
+        Movment.Move(m_VerticalVelocity * Time.deltaTime);
+
+        SmoothMomentum();
         SmoothMovment();
 
-        Movment.Move(m_SmoothMoveDirection * m_MovementSpeed * Time.deltaTime);
+        MoveMotor();
 
+        m_WasGrounded = m_IsGrounded;
+        LastYVelocity = m_VerticalVelocity.y;
+    }
+
+    void CheckGround()
+    {
+        if (Physics.CheckSphere(transform.position + (Vector3.up * 0.2f),0.3f,GroundMask))
+        {
+           m_IsGrounded = true; 
+        }
+        else
+        {
+            m_IsGrounded = false; 
+        }
     }
 
     void SmoothMovment()
     {
-        if (m_IsSprinting)
+        if (!m_IsGrounded)
         {
-            m_MovementSpeed = Mathf.Lerp(m_MovementSpeed,SprintSpeed, Time.deltaTime * Acceleration);
+            m_MovementSpeed = Mathf.Lerp(m_MovementSpeed, AirSpeed, Time.deltaTime * Acceleration);
+        }
+        else if (m_MoveDirection == Vector3.zero)
+        {
+            m_MovementSpeed = Mathf.Lerp(m_MovementSpeed, 0, Time.deltaTime * Acceleration);
         }
         else
         {
-            m_MovementSpeed = Mathf.Lerp(m_MovementSpeed,WalkSpeed, Time.deltaTime * Acceleration);
+            m_MovementSpeed = Mathf.Lerp(m_MovementSpeed, MoveSpeed, Time.deltaTime * Acceleration);
         }
 
         m_SmoothMoveDirection = Vector3.Lerp(m_SmoothMoveDirection, new Vector3(m_MoveDirection.x,0,m_MoveDirection.z).normalized, Time.deltaTime * Acceleration);
+
+        CamEffects.RotateTo += new Vector3(m_ForwardMovement,0,-m_SidewardMovement) * 10 * Time.deltaTime;
+    }
+
+    void SmoothMomentum()
+    {
+        if (m_IsGrounded)
+        {
+            m_MomentumDirection = Vector3.Lerp(m_MomentumDirection,Vector3.zero, Time.deltaTime * 10);
+        }
+        else
+        {
+            m_MomentumDirection = Vector3.Lerp(m_MomentumDirection, m_SmoothMoveDirection.normalized * m_MomentumDirection.magnitude, Time.deltaTime * 1);
+            m_MomentumDirection = Vector3.Lerp(m_MomentumDirection, Vector3.zero, Time.deltaTime * m_MomentumDecay);
+        }
     }
 
 
-    void GetInput()
+    void GetMovementInput()
     {
         m_SidewardMovement = Input.GetAxisRaw("Horizontal");
         m_ForwardMovement = Input.GetAxisRaw("Vertical");
 
         m_MoveDirection = Orientation.forward * m_ForwardMovement + Orientation.right * m_SidewardMovement;
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            m_IsSprinting = true;
-        }
-
-        if (m_SmoothMoveDirection == Vector3.zero)
-        {
-            m_IsSprinting = false;
-        }
     }
+
+    void MoveMotor()
+    {
+        Movment.Move(m_SmoothMoveDirection * m_MovementSpeed * Time.deltaTime);
+        Movment.Move(m_MomentumDirection * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        if (m_IsGrounded)
+        {
+            m_VerticalVelocity.y = 0;
+
+            if (!m_WasGrounded)
+            {
+                CamEffects.RotateTo.x += Mathf.Abs(LastYVelocity);
+            }
+        }
+        else
+        {
+            m_VerticalVelocity.y -= m_Gravity * Time.deltaTime;
+
+            if (m_WasGrounded)
+            {
+                m_MomentumDirection = m_SmoothMoveDirection * m_MovementSpeed;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_VerticalVelocity.y = Mathf.Sqrt(2 * m_JumpHeight * m_Gravity);
+            CamEffects.RotateTo.x += 5;
+            m_MomentumDirection += m_SmoothMoveDirection * m_JumpMomentum;
+        }
+
+        float VerticalTilt = m_VerticalVelocity.y;
+        VerticalTilt = Mathf.Clamp(VerticalTilt, -45,10);
+        CamEffects.RotateTo.x += VerticalTilt * Time.deltaTime;
+    }
+
+    
 }
