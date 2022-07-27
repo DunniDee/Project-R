e8f4fe42b31bebc6bafb92921c912e8c42f04774
Ashley <Ashley.rickit@gmail.com>
2022-07-27 21:32:47 +1200

    Timetrial Thing

--- Assets/Scr_TimeTrial.cs
+++ Assets/Scr_TimeTrial.cs
@@ -1,22 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.UI;
 public class Scr_TimeTrial : MonoBehaviour
 {
+    public TMP_Text TimeText;
+    public TMP_Text curTimeText;
+    public TMP_Text finaltext;
+    public TMP_Text finalTimeText;
+
     public float Timer = 0.0f;
     bool isTrialStarted = false;
-
+    bool isFading = true;
     public void StartTimer()
     {
-        isTrialStarted = true;
+        if (isTrialStarted == false)
+        {
+            isTrialStarted = true;
+            Timer = 0f;
+            FadeIn(TimeText, 1.0f);
+            FadeIn(curTimeText, 1.0f);
+        }
+       
     }
 
     public void StopTimer()
     {
-        isTrialStarted = false;
+        if (isTrialStarted == true)
+        {
+            isTrialStarted = false;
+        }
+
     }
 
+    public void FadeIn(TMP_Text _text,float _FadeTime)
+    {
+        if (isFading)
+        {
+            _text.CrossFadeAlpha(1.0f, _FadeTime, true);
+        }
+        
+    }
+
+    public void FadeOut(TMP_Text _text, float _FadeTime)
+    {
+        _text.CrossFadeAlpha(0.0f, _FadeTime, true);
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -28,7 +58,10 @@ public class Scr_TimeTrial : MonoBehaviour
     {
         if (isTrialStarted)
         {
+            FadeIn(TimeText, 1.0f);
+            FadeIn(curTimeText, 1.0f);
             Timer += Time.deltaTime;
+            curTimeText.text = Timer.ToString("F2");
         }
 
     }
--- Assets/Scripts/Scr_EnemyProjectile.cs
+++ Assets/Scripts/Scr_EnemyProjectile.cs
@@ -10,7 +10,7 @@ public class Scr_EnemyProjectile : MonoBehaviour
     {
         if(collision.rigidbody.tag == "Player")
         {
-            collision.rigidbody.GetComponent<Scr_PlayerHealth>().TakeDamage(m_fDamage);
+           // collision.rigidbody.GetComponent<Scr_PlayerHealth>().TakeDamage(m_fDamage);
             Debug.Log("Damage Done " + m_fDamage);
             Disable();
         }
