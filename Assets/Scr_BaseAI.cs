0dd2a5d6c5d187e9e1ecbecfeb96aa890eb87cec
Dunstan <dunstanbdalin@gmail.com>
2022-07-31 14:55:59 +1200

    WIP

---
+++ Assets/Scr_BaseAI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_BaseAI : MonoBehaviour
+{
+    public enum State
+    {
+        Idle,
+        Attack,
+        Dead,
+    }
+
+    [SerializeField]
+    protected 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
