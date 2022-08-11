5e39ec6eb895d6ff73f09da407876833db40811d
Noobcoodoocoo <noobcoodoocoo@gmail.com>
2022-08-12 11:00:17 +1200

    setdressing stuff

---
+++ Assets/TurnOffOnPlay.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffOnPlay : MonoBehaviour
+{
+    public GameObject ThingToTurnOff;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ThingToTurnOff.SetActive(false);
+        //pissabqabbkfdsbkjsdf
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
