d5830756356aaf49bbf88598b23cd24ba8a3ff8a
Ashley <Ashley.rickit@gmail.com>
2022-08-11 23:51:13 +1200

    UpgradeShop

--- Assets/Scripts/Scr_UpgradeShop.cs
+++ Assets/Scripts/Scr_UpgradeShop.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Cinemachine;
+using System;
 
 public class Scr_UpgradeShop : MonoBehaviour
 {
@@ -11,12 +13,10 @@ public class Scr_UpgradeShop : MonoBehaviour
     [Space]
     public bool isShopActive = false;
 
-    public void EnableShopCanvas(bool _b)
-    {
-        UpgradeShop_Canvas.SetActive(_b);
-        isShopActive = _b;
-    }
-    
+    public CinemachineVirtualCamera vcam;
+
+   
+
     [System.Serializable]
     public struct ShopWeaponUI
     {
@@ -26,7 +26,43 @@ public class Scr_UpgradeShop : MonoBehaviour
         public TMP_Text magazineSize;
 
     }
+
     public List<ShopWeaponUI> ShopSlots;
+
+    public void EnableShopCanvas(bool _b)
+    {
+        UpgradeShop_Canvas.SetActive(_b);
+        isShopActive = _b;
+        if (_b == true)
+        {
+            vcam.Priority = 10;
+        }
+        else {
+            vcam.Priority = 9;
+        }
+    }
+    private void Init()
+    {
+        int i = 0; 
+        foreach (Script_PlayerStatManager.WeaponStats weapon in Script_PlayerStatManager.Instance.WeaponStatList)
+        {
+            /*Script_PlayerStatManager.WeaponStats weaponStats = Script_PlayerStatManager.Instance.WeaponStatList.ToArray()[i];*/
+            SetWeaponShopSlot(weapon, i);
+            i++;
+        }
+    }
+
+    private void SetWeaponShopSlot(Script_PlayerStatManager.WeaponStats weaponStats,int i)
+    {
+        ShopSlots.Add(new ShopWeaponUI());
+
+        ShopSlots[i].GunName.text = weaponStats.WeaponName;
+        ShopSlots[i].Damage.text = weaponStats.Modified_Damage.ToString();
+        ShopSlots[i].magazineSize.text = weaponStats.Modified_MaxAmmo.ToString();
+
+        ShopSlots[i].Cost.text = "100";
+    }
+
 
     private void Start()
     { 
@@ -34,14 +70,14 @@ public class Scr_UpgradeShop : MonoBehaviour
         {
             Debug.LogWarning("Missing UpgradeShopCanvas");
         }
-     
+        Init();
 
         
     }
 
     private void Update()
     {
-        if (isShopActive)
+        if (isShopActive && Input.GetKeyDown(KeyCode.Tab))
         {
             EnableShopCanvas(false);
         }
