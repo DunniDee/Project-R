d3be6601f16d0418fe52672823e0cca112d6179c
Noobcoodoocoo <noobcoodoocoo@gmail.com>
2022-10-23 12:39:34 +1300

    lightining finniahs stuff

--- Assets/TpTo.cs
+++ Assets/TpTo.cs
@@ -4,27 +4,47 @@ using UnityEngine;
 
 public class TpTo : MonoBehaviour
 {
+    public GameObject Obj;
     public Transform Destination;
     public Transform SelfTransform;
+    public float X = 0f;
+    public float Y = 0f;
+    public float RandRange = 1000f;
+    public float LookAt1 = 0f;
+    public float LookAt2 = -600f;
+    public float LookAt3 = 0f;
+    public float TurnOnTime = 1f;
 
-    // Start is called before the first frame update
     void Start()
     {
-
-            StartCoroutine(Example4());
-
-     
+        StartCoroutine(Example4());
+        StartCoroutine(Example3());
     }
 
     IEnumerator Example4()
     {
-
         yield return new WaitForSeconds(2.5f);
-        SelfTransform.transform.position = Destination.transform.position;
+
+        X = Random.Range(RandRange, -RandRange);
+        Y = Random.Range(RandRange, -RandRange);
+        SelfTransform.transform.position = new Vector3(X, -600f, Y);
+        transform.LookAt(new Vector3(LookAt1, LookAt2, LookAt3));
+        var lookPos = Destination.position - transform.position;
+
+
         StartCoroutine(Example4());
 
 
 
+    }
+
+    IEnumerator Example3()
+    {
+        yield return new WaitForSeconds(TurnOnTime);
+        MeshRenderer m = Obj.GetComponent<MeshRenderer>();
+        m.enabled = true;
+        Animator A = Obj.GetComponent<Animator>();
+        A.enabled = true;
     }
 }
 
