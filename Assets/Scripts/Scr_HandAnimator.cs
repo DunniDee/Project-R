bc57b5950b07d83fcca830b44941c588cb0eb5e8
Dunstan <dunstanbdalin@gmail.com>
2022-10-23 16:24:14 +1300

    Meele Now Works

--- Assets/Scripts/Scr_HandAnimator.cs
+++ Assets/Scripts/Scr_HandAnimator.cs
@@ -6,12 +6,18 @@ public class Scr_HandAnimator : MonoBehaviour
 {
     [SerializeField] Scr_CameraEffects CameraEffects;
     [SerializeField] Scr_HandAnimator HandEffects;
+    [SerializeField] script_WeaponSwap WeaponSwap;
     public Vector3 RotateTo;
     // Update is called once per frame
+
+    /// <summary>
+    /// Apllies a secondary motion to the hands
+    /// </summary>
     void Update()
     {
         RotateTo = Vector3.Lerp(RotateTo,CameraEffects.RotateTo, Time.deltaTime * 5);
 
         transform.localRotation = Quaternion.Euler(RotateTo);
+        WeaponSwap.SetActiveAnim(false);
     }
 }
