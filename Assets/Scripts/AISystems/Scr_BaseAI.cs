ebcbca72953822c5dfafd1b56a5930d93a7f87f9
Ashley <Ashley.rickit@gmail.com>
2022-11-01 15:14:57 +1300

    Cultist Hitboxes are bigger, Removed style meter from ScreenSpace UI
    Shooter hitbox is bigger

--- Assets/Scripts/AISystems/Scr_BaseAI.cs
+++ Assets/Scripts/AISystems/Scr_BaseAI.cs
@@ -68,16 +68,12 @@ public class Scr_BaseAI : MonoBehaviour, IDamageable
         {
             case CustomCollider.DamageType.Critical:
                 m_Health -= _Damage * 2;
-                Scr_DamagePopupManager.Instance.DisplayDamagePopup((int)50.0f, DamagePopupPos);
-                //Scr_DamagePopupManager.Instance.CreateHealthOrb(this.gameObject.transform);
-                scr_GameManager.i.IncreaseTotalScore(50.0f);
-                Scr_StyleManager.i.IncreaseStylePoints(50.0f);
+                /*Scr_DamagePopupManager.Instance.DisplayDamagePopup((int)50.0f, DamagePopupPos);*/
+
                 break;
             case CustomCollider.DamageType.Normal:
                 m_Health -= _Damage;
-                Scr_DamagePopupManager.Instance.DisplayDamagePopup((int)10.0f, DamagePopupPos);
-                scr_GameManager.i.IncreaseTotalScore(10.0f);
-                Scr_StyleManager.i.IncreaseStylePoints(10.0f);
+                /*Scr_DamagePopupManager.Instance.DisplayDamagePopup((int)10.0f, DamagePopupPos);*/
             break;
         }
 
