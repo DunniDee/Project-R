e8d704542d5b42a02e9acffd3c5362c3c922b0b4
Ashley <Ashley.rickit@gmail.com>
2022-11-04 12:45:52 +1300

    Merge branch 'Staging' of https://github.com/DunniDee/Project-R into Staging

--- Assets/Scripts/ScriptedEvent Systems/Scr_LaunchPad.cs
+++ Assets/Scripts/ScriptedEvent Systems/Scr_LaunchPad.cs
@@ -6,12 +6,16 @@ public class Scr_LaunchPad : MonoBehaviour
 {
     [SerializeField] float Force;
     [SerializeField] float Height;
+
+    [SerializeField] AudioSource AS;
+    [SerializeField] AudioClip BoostSound;
     private void OnTriggerEnter(Collider other) 
     {
         Debug.Log(other);
         if (other.CompareTag("Player"))
         {
             other.GetComponent<Scr_PlayerMotor>().Launch(transform.forward * Force, Height);
+            AS.PlayOneShot(BoostSound);
         }
     }
 }
