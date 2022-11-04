c8784cc9f590e472d656c211cbebdb87772e4328
Ashley <Ashley.rickit@gmail.com>
2022-11-04 13:53:43 +1300

    AI Changes to 0, BoilerRoom Changes,  Updated Scr_BoilerRoom

--- Assets/Scripts/ScriptedEvent Systems/scr_BoilerRoom.cs
+++ Assets/Scripts/ScriptedEvent Systems/scr_BoilerRoom.cs
@@ -8,6 +8,7 @@ using TMPro;
 public class scr_BoilerRoom : MonoBehaviour
 {
     [Header("Internal Components")]
+    public Animator UIAnimator;
     public GameObject ArenaCanvas;
     public TMP_Text Timer;
     public UnityEvent OnRoomCompletedEvent;
@@ -22,37 +23,58 @@ public class scr_BoilerRoom : MonoBehaviour
     [SerializeField] List<GameObject> AI_Prefabs;
 
     [SerializeField] List<GameObject> InstantiatedAI;
-    bool isArenaActive = false;
+    [SerializeField] bool isArenaActive = false;
+
+    public void Start()
+    {
+        
+    }
 
     private IEnumerator SpawnAICoroutine()
     {
         int EnemiesToSpawn = maxAmountEnemies - InstantiatedAI.Count;
         for (int i = 0; i < EnemiesToSpawn; i++)
         {
-            int k = Random.Range(0, AI_Prefabs.Count);
-            int j = Random.Range(0, SpawnPosition.Count);
-            InstantiatedAI.Add(Instantiate(AI_Prefabs[k], SpawnPosition[j], false));
+      
             yield return new WaitForSeconds(1.0f);
         }
     }
 
+    private void UpdateInstancedAI() {
+        int i = 0;
+        foreach (GameObject AI in InstantiatedAI)
+        {
+            i++;
+            if (AI == null)
+            {
+                InstantiatedAI.RemoveAt(i);
+            }
+        }
+    }
     private void UpdateTimer()
     {
-        currentTime -= Time.deltaTime;
+         currentTime -= Time.deltaTime;
         Timer.text = "Time Till Explosion 00:" + currentTime.ToString("F0");
     }
     public void StartFinalEvent()
     {
         ArenaCanvas.SetActive(true);
+        UIAnimator.SetTrigger("FadeIn");
         StartCoroutine(SpawnAICoroutine());
         isArenaActive = true;
         currentTime = MaxTime;
     }
 
+    public void SpawnAI()
+    {
+        int k = Random.Range(0, AI_Prefabs.Count);
+        int j = Random.Range(0, SpawnPosition.Count);
+        InstantiatedAI.Add(Instantiate(AI_Prefabs[k], SpawnPosition[j], false));
+    }
     public void FinishFinalEvent()
     {
         ArenaCanvas.SetActive(false);
-        //StartCoroutine(SpawnAICoroutine());
+        OnRoomCompletedEvent.Invoke();
         isArenaActive = false;
     }
 
@@ -60,11 +82,11 @@ public class scr_BoilerRoom : MonoBehaviour
     {
         if(isArenaActive)
         {
-            
+            UpdateInstancedAI();
             UpdateTimer();
             if (InstantiatedAI.Count < maxAmountEnemies)
             {
-                SpawnAICoroutine();
+                SpawnAI();
             }
 
             if (currentTime <= 0)
